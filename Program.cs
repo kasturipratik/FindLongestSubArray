4a4531e848d3df0eb2f14f5df9ead37c5e73de17
kasturipratik <pratik.parajuli@yahoo.com>
2021-10-27 03:15:07 -0400

    added comments

--- Program.cs
+++ Program.cs
@@ -4,40 +4,30 @@ namespace FindLongestSubArray
 {
     class Program
     {
+        // testing solution
         static void Main(string[] args)
         {
-
             RunApplication();
         }
 
-        static void RunApplication()
+        // check if one list is sub set of other
+        static bool IsSubSetOf(int[] A, int[] B, int[] C)
         {
-            // test 1
-            var a = new int[] { 1, 1, 5, 1, 2 };
-            var b = new int[] { 1, 2 };
-            var c = new int[] { 2, 1 };
-            PrintResult(IsContignuousSubArray(a, b, c));
+            if (B.Length > A.Length ||
+                B.Length > C.Length)
+            {
+                return false;
+            }
 
-            // test 2
-            var a1 = new int[] { 11, 14, 1, 2, 17, 2, 11, 6, 11, 22, 14, 2, 17 };
-            var b1 = new int[] { 11, 22, 14, 1 };
-            var c1 = new int[] { 14, 22, 11, 1, 2 };
-            PrintResult(IsContignuousSubArray(a1, b1, c1));
-            
-            // test 3
-            var a12 = new int[] { 11, 14, 1, 2, 17, 2, 11, 6, 11, 22, 14, 1, 2, 17 };
-            var b12 = new int[] { 11, 22, 14, 1 };
-            var c12 = new int[] { 14, 22, 11, 1 };
-            PrintResult(IsContignuousSubArray(a12, b12, c12));
-
-        }
-        static void PrintResult(bool x)
-        {
-            Console.WriteLine((x) ? x.ToString()
-                                  : "No Match found \n" + x.ToString());
+            for (int b = 0; b < B.Length; b++)
+            {
+                var flag = Array.Exists(C, c => c == B[b]);
+                if (!flag) { return false; }
+            }
+            return true;
         }
 
-
+        // main logic to check if array B is contignuous sub array of A or not
         static bool IsContignuousSubArray(int[] A, int [] B, int[] C)
         {
 
@@ -76,21 +66,35 @@ namespace FindLongestSubArray
             return flag;
         }
 
-        // check if one list is sub set of other
-        static bool IsSubSetOf(int[]A, int[] B, int[] C)
+        // data for testing
+        static void RunApplication()
         {
-            if (B.Length > A.Length ||
-                B.Length > C.Length )
-            {
-                return false;
-            }
+            // test 1
+            var a = new int[] { 1, 1, 5, 1, 2 };
+            var b = new int[] { 1, 2 };
+            var c = new int[] { 2, 1 };
+            PrintResult(IsContignuousSubArray(a, b, c));
 
-            for (int b = 0; b < B.Length; b++)
-            {
-                var flag = Array.Exists(C, c => c == B[b]);
-                if (!flag) { return false; }
-            }
-            return true;
+            // test 2
+            var a1 = new int[] { 11, 14, 1, 2, 17, 2, 11, 6, 11, 22, 14, 2, 17 };
+            var b1 = new int[] { 11, 22, 14, 1 };
+            var c1 = new int[] { 14, 22, 11, 1, 2 };
+            PrintResult(IsContignuousSubArray(a1, b1, c1));
+
+            // test 3
+            var a12 = new int[] { 11, 14, 1, 2, 17, 2, 11, 6, 11, 22, 14, 1, 2, 17 };
+            var b12 = new int[] { 11, 22, 14, 1 };
+            var c12 = new int[] { 14, 22, 11, 1 };
+            PrintResult(IsContignuousSubArray(a12, b12, c12));
+
         }
+
+        // print result
+        static void PrintResult(bool x)
+        {
+            Console.WriteLine((x) ? x.ToString()
+                                  : "No Match found \n" + x.ToString());
+        }
+
     }
 }
